211e01cd7cc90d323209688cb30b4696123ec286
Antonio <antoniobriceno568@gmail.com>
2025-01-25 10:39:15 +0100

    Simulando un fetch de los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
+import { use } from "react";
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  {
+    id: 1,
+    userName: "midudev",
+    name: "Miguel Angel Durán",
+    isFollowing: true,
+  },
+  {
+    id: 2,
+    userName: "pheralb",
+    name: "Pablo H.",
+    isFollowing: true,
+  },
+  {
+    id: 3,
+    userName: "PacoHdezs",
+    name: "Paco Hdez",
+    isFollowing: false,
+  },
+  {
+    id: 4,
+    userName: "TMchein",
+    name: "Tomas",
+    isFollowing: false,
+  },
+];
 export function App() {
   return (
-    <>
-      <TwitterFollowCard
-        userName="midudev"
-        name="Miguel Angel Duran"
-        initialIsFollowing={true}
-      />
-      <TwitterFollowCard
-        userName="holamundo"
-        name="Hola Mundo"
-        initialIsFollowing={false}
-      />
-    </>
+    <section className="App">
+      {users.map(({ id, userName, name, isFollowing }) => {
+        return (
+          <TwitterFollowCard
+            key={id}
+            userName={userName}
+            name={name}
+            initialIsFollowing={isFollowing}
+          ></TwitterFollowCard>
+        );
+      })}
+    </section>
   );
 }
